d02206234b468536c02f055d5047ea467edd67dd
oliver <oliver.meek2@gmail.com>
2025-01-21 15:55:23 +0000

    finally did it

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+
+#%%
+print ("hello")
+# %%
+import pandas as pd
+# Load dataset
+data = pd.read_csv('/workspaces/DS-3021/data/beaches.csv')
+
+# Display the first few rows
+print(data.head())
+# %%
+
+
---
+++ practice.py
@@ -0,0 +1,14 @@
+
+#%%
+print ("hello")
+# %%
+import pandas as pd
+import wrangler as wr
+
+# Load dataset
+data = pd.read_csv('/workspaces/DS-3021/data/beaches.csv')
+
+# Display the first few rows
+print(data.head())
+# %%
+
